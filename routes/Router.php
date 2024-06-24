ec5462199bde0b938f8d20af45a8c441a9b4c45d
rabaasourour <so.gomgom@gmail.com>
2024-06-24 21:13:33 +0200

    first commit ( add a work space)

---
+++ routes/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+class Router {
+    public $url;
+    public $routes =[];
+
+    // Corrected constructor
+    public function __construct($url) {
+        $this->url = trim ($url, '/');
+    }
+
+    public function get(string $path, string $action){
+    
+        $this->routes['GET'][]= new Route($path, $action);
+    }
+
+    public function run(){
+        foreach ($this->routes($_SERVER['REQUEST_METHOD']) as $route){
+            if ($route->matches($this->url)){
+            $route->execute();
+            }
+        }
+
+        return header('HTTP/1.0 404 Not Found');
+    }
+}
