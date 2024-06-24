3b0fc187e1f13c5e81b3248e8f400b36435e4623
rabaasourour <so.gomgom@gmail.com>
2024-06-24 14:37:49 +0200

    Initial commit

---
+++ environnement/workspace/Arcadia/arcadia-Back/src/src/Entity.php
@@ -0,0 +1,39 @@
+// src/Entity/Product.php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+    // getters and setters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
